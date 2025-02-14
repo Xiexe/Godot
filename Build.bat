fb7cb7106f019ad32a59bdad7e307d9df6ad34ae
Xiexe <thexiexe@gmail.com>
2025-02-13 22:46:36 -0500

    full build script

--- Build.bat
+++ Build.bat
@@ -1,5 +1,7 @@
 dotnet nuget add source "%~dp0LocalNugetSource" --name LocalNugetSource
 
+scons platform=windows
+
 "bin/godot.windows.editor.double.x86_64.mono.exe" --headless --generate-mono-glue modules/mono/glue
 
 "./modules/mono/build_scripts/build_assemblies.py" --godot-output-dir="./bin" --push-nupkgs-local LocalNugetSource --precision=double
